2f2a50bc74165a0f2c0962e39ea7ef07809dfc4f
Ivan Mazhara <imazharagithub@gmail.com>
2025-07-25 23:36:32 +0200

    Add DTO for Employee

---
+++ HRManagementSystem/Models/DTOs/EmployeeDto.cs
@@ -0,0 +1,17 @@
+﻿namespace HRManagementSystem.Models.DTOs;
+
+public class EmployeeDto
+{
+    public int EmployeeId { get; set; }
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    
+    public decimal? Salary { get; set; }
+    public string Email { get; set; } = null!;
+    public DateTime HireDate { get; set; }
+    
+    public int JobId { get; set; }
+    public int DepartmentId { get; set; }
+    public string? JobTitle { get; set; }
+    public string? DepartmentName { get; set; }
+}
